effb2bc0cc5952538163052786f00234670067fd
Berat Yıldız <yildiz.berat@protonmail.com>
2025-08-14 15:28:21 +0300

    Update analyzer.py
    Developed express_parser.py and some error fixed in analyzer.py

--- authgraph/core/analyzer.py
+++ authgraph/core/analyzer.py
@@ -1,11 +1,14 @@
-
 from authgraph.scanner.file_scanner import find_js_files
 from authgraph.scanner.express_parser import parse_express_code
 
 def analyze_project(path):
     all_routes = []
     for file in find_js_files(path):
-        with open(file, "r", encoding="utf-8") as f:
-            content = f.read()
-            all_routes.extend(parse_express_code(content))
+        try:
+            with open(file, "r", encoding="utf-8") as f:
+                content = f.read()
+            all_routes.extend(parse_express_code(content, filename=file))
+        except Exception as e:
+            # İstersen logla
+            pass
     return all_routes
